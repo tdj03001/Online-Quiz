7666fe592e09cd826861a0fb0aaed80be13c743f
Tyler Jenkins <tyjenki@gmail.com>
2019-12-11 00:46:56 -0500

    initial stab at timer. For some reason it's stopping at 2 seconds

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 function flip() {
-  document.getElementById("flip").style.display = "block";
+  document.getElementById("quizBlock").style.display = "block";
+  document.getElementById("landingPage").style.display = "none";
 }
 
-var totalSeconds = 60;
-var minutes = parseInt(totalSeconds / 60);
-var seconds = parseInt(totalSeconds % 60);
+
+function showTimer() {
+  document.getElementById("timer").innerHTML = "Time Remaining: 0:00";
+}
+
 
 function startTimer() {
-  document.getElementById("timer").innerHTML = "Time Remaining: " + minutes + " minutes" + " " + seconds + " seconds";
+  var seconds = 60;
+  var timer = setInterval(function() {
+      document.getElementById('timer').innerHTML="Time Remaining: " + seconds + " seconds";
+      seconds--;
+      if (seconds <= 0) {
+        clearInterval(timer);
+        alert("Time's Up! Your score is 0");
+          
+      }
+  }, 1000);
 }
+
+//research setInterval, clearInterval, and setTimeout for timer improvements
+
+
+// var totalSeconds = 60;
+// var minutes = parseInt(totalSeconds / 60);
+// var seconds = parseInt(totalSeconds % 60);
+
+// function startTimer() {
+//   document.getElementById("timer").innerHTML = "Time Remaining: " + minutes + " minutes" + " " + seconds + " seconds";
+// }
 //   if (totalSeconds <= 0) {
 //     stopTimer();
 //   } else {
@@ -23,6 +46,4 @@ function startTimer() {
 //   document.getElementById("timer").innerHTML = "Out of time";
 // };
 
-function showTimer() {
-  document.getElementById("timer").innerHTML = "Time Remaining: 0";
-}
+
