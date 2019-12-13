4c13524d929775fd31324cde9cac7a282a0b47c1
Tyler Jenkins <tyjenki@gmail.com>
2019-12-12 23:57:39 -0500

    final tweaks

--- script.js
+++ script.js
@@ -2,25 +2,25 @@
 function flip1() {
   document.getElementById("quizBlock1").style.display = "block";
   document.getElementById("landingPage").style.display = "none";
-}
+};
 
  //Call this function when any answer selected on Question 1
 function flip2() {
   document.getElementById("quizBlock1").style.display = "none";
   document.getElementById("quizBlock2").style.display = "block";
-}
+};
 
 //Call this function when any answer selected on Question 2
 function flip3() {
   document.getElementById("quizBlock2").style.display = "none"; 
   document.getElementById("quizBlock3").style.display = "block";
-}
+};
 
 //Call this function when any answer selected on Question 3
 function flip4() {
   document.getElementById("quizBlock3").style.display = "none"; 
   document.getElementById("quizBlock4").style.display = "block";
-}
+};
 
 //Call this function when any answer selected on Question 4
 function flip5() {
@@ -29,71 +29,52 @@ function flip5() {
   document.getElementById("quizBlock3").style.display = "none";
   document.getElementById("quizBlock4").style.display = "none";
   document.getElementById("summary").style.display = "block";
+  // startTimer().clearInterval(timer);  I was trying to access the "timer" variable in startTimer() so I could stop the clock when flip5() ran. flip5() runs when any button on the 4th card is clicked.
   var delayInMilliseconds = 1000;
 
   setTimeout(function() {
     document.getElementById("notifier").style.display = "none";
   }, delayInMilliseconds);
-}
+  // return true;
+};
 
 //This is called on page load
 function showTimer() {
   document.getElementById("timer").textContent = ("Time Remaining: 0:00");
-}
+};
+
 
 
 //This is called when Start Quiz button is clicked
 function startTimer() {
-  var seconds = 10;
+  var seconds = 60;
   var timer = setInterval(function() {
-      document.getElementById('timer').textContent="Time Remaining: " + seconds + " seconds ";
-      seconds--;
+    document.getElementById("timer").textContent="Time Remaining: " + seconds + " seconds ";
+    seconds--;
       if (seconds === 0) {
-        // clearInterval(timer);
-        alert("Time's Up! Your score is 0!");
-        confirm("Try again?") 
-          if (true) {
-            parent.window.location.reload(true);
-          }          
-      }
-  }, 1000);
-}
+        clearInterval(timer);
+        flip5(); //because of the delayInMilliseconds where this function is declared above, the alert immediately below here runs before the "summary" card shows up.
+        alert("Time's Up! Your score is 2!"); //somehow this alert always comes up when the timer is at 2 seconds, then when the alert is cleared it drops to 1 second...
+    
+    var delayInMilliseconds = 10000;
+    setTimeout(function() {
+      confirm("Thanks for playing! Try again?")
+          parent.window.location.reload(true);
+      
+  }, delayInMilliseconds);
+  
+  }
+}, 1000);
+
+};
 
 function correctAnswer() {
     document.getElementById("notifier").style.display = "block";
-    document.getElementById("notifier").textContent = "Correct!"; 
-}
+    document.getElementById("notifier").textContent = "Previous answer correct!"; 
+};
 
 function incorrectAnswer() {
   document.getElementById("notifier").style.display = "block";
-  document.getElementById("notifier").textContent = "Incorrect!";
+  document.getElementById("notifier").textContent = "Previous answer incorrect!";
  
-}
-
-
-
-//research setInterval, clearInterval, and setTimeout for timer improvements
-
-
-// var totalSeconds = 60;
-// var minutes = parseInt(totalSeconds / 60);
-// var seconds = parseInt(totalSeconds % 60);
-
-// function startTimer() {
-//   document.getElementById("timer").textContent = "Time Remaining: " + minutes + " minutes" + " " + seconds + " seconds";
-// }
-//   if (totalSeconds <= 0) {
-//     stopTimer();
-//   } else {
-//     totalSeconds -= 1;
-//     minutes = parseInt(totalSeconds / 60);
-//     seconds = parseInt(totalSeconds % 60);
-//     stopTimer("StartTimer()", 1000);
-//   }
-// }
-
-// function stopTimer() {
-//   document.getElementById("timer").textContent = "Out of time";
-// };
-
-
+};
